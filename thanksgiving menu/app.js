2e3924ac3fa40682c4aa8386691b422f38caf4b4
paxtongreen <88725740+paxtongreen@users.noreply.github.com>
2021-11-23 15:28:32 -0600

    Add files via upload

---
+++ thanksgiving menu/app.js
@@ -0,0 +1,16 @@
+let main = document.getElementById('main');
+let output = document.getElementById('output')
+
+main.addEventListener('addToPlates', (e) => {
+    e.preventDefault();
+
+    let sweetTea = 1.99;
+    let lemonade = 1.99;
+    let dressing = 3.99;
+    let ham = 7;
+    let macNCheese = 5;
+    let greens = 4.99;
+    let pie = 5.99;
+    let yams = 4.99;
+
+})
